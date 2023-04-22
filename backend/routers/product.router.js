9ceeedd0d6456fab2142f00504af7f7df726b87e
tanersaydam <tanersaydam@gmail.com>
2023-04-22 15:04:53 +0300

    Ders 35: Product GetList Metodu Yazalım

--- backend/routers/product.router.js
+++ backend/routers/product.router.js
@@ -42,3 +42,43 @@ router.post("/removeById", async (req, res)=> {
         res.json({message: "Ürün kaydı başarıyla silindi!"});
     });
 });
+
+//Ürün Listesi Getir
+router.post("/", async(req, res)=> {
+    response(res, async()=> {
+        const {pageNumber, pageSize, search} = req.body;
+
+        let productCount = await Product.find({
+            $or: [
+                {
+                    name: { $regex: search, $options: 'i'}
+                }
+            ]
+        }).count();
+
+        let products = await Product
+        .find({
+            $or: [
+                {
+                    name: { $regex: search, $options: 'i'}
+                }
+            ]
+        })
+        .sort({name: 1})
+        .populate("categories")
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize);
+
+        let totalPageCount = Math.ceil(productCount / pageSize);
+        let model = {
+            datas: products,
+            pageNumber: pageNumber,
+            pageSize: pageSize,
+            totalPageCount: totalPageCount,
+            isFirstPage: pageNumber == 1 ? true : false,
+            isLastPage: totalPageCount == pageNumber ? true : false
+        };
+
+        res.json(model);
+    });
+});
